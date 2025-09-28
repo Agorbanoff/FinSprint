210f6a5dc8a1880043c8f9ca1ddc1d2f5a1e2402
Agorbanoff <alexandargorbanov@gmail.com>
2025-09-28 18:35:49 +0300

    added some file

---
+++ Backend/src/main/java/com/finsprint/FinSprintApplication.java
@@ -0,0 +1,13 @@
+package com.finsprint;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinSprintApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinSprintApplication.class, args);
+	}
+
+}
